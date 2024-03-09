2f5f3489d95be9ccd8746678b0e4df0a1f9100b4
henry anand <13521004@std.stei.itb.ac.id>
2024-03-09 22:39:44 +0700

    feat : add splash screen

--- app/src/main/java/com/example/bondoman/MainActivity.kt
+++ app/src/main/java/com/example/bondoman/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.bondoman
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -15,9 +16,9 @@ private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-     binding = ActivityMainBinding.inflate(layoutInflater)
-     setContentView(binding.root)
+        installSplashScreen()
+         binding = ActivityMainBinding.inflate(layoutInflater)
+         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
 
