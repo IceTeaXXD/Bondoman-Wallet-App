9e747004d8ecdf89d219579a7c4c6e5df2b3ef26
henry anand <13521004@std.stei.itb.ac.id>
2024-03-10 02:34:50 +0700

    feat : add simple donut chart placeholder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -51,6 +50,9 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.preference:preference:1.2.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
+	implementation("com.diogobernardino:williamchart:3.10.1")
+    implementation("com.diogobernardino.williamchart:tooltip-slider:3.10.1")
+    implementation("com.diogobernardino.williamchart:tooltip-points:3.10.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
