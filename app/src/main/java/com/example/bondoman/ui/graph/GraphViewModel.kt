7bfbcafd484edd3ecc6476f4ef6a1ab7d0b08f15
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2024-03-10 00:42:25 +0700

    feat: bottom nav

---
+++ app/src/main/java/com/example/bondoman/ui/graph/GraphViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.bondoman.ui.graph
+
+import androidx.lifecycle.ViewModel
+
+class GraphViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/bondoman/ui/transactions/TransactionsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.bondoman.ui.transactions
+
+import androidx.lifecycle.ViewModel
+
+class TransactionsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
