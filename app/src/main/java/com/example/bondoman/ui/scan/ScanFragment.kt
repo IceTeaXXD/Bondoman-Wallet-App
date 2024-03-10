951f5c5ec8a6978dc5cda1ed27a5d4ea5fa80b11
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2024-03-10 20:12:14 +0700

    feat: camera functionality

--- app/src/main/java/com/example/bondoman/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/example/bondoman/ui/scan/ScanFragment.kt
@@ -1,12 +1,22 @@
 package com.example.bondoman.ui.scan
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageButton
 import com.example.bondoman.R
+import androidx.databinding.DataBindingUtil
+import com.example.bondoman.databinding.FragmentScanBinding
 
 class ScanFragment : Fragment() {
 
@@ -14,19 +24,32 @@ class ScanFragment : Fragment() {
         fun newInstance() = ScanFragment()
     }
 
-    private lateinit var viewModel: ScanViewModel
+    private lateinit var binding: FragmentScanBinding
+    private val REQUEST_IMAGE_CAPTURE = 100
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_scan, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_scan, container, false)
+        binding.cameraButton.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            } catch (e: Exception) {
+                Toast.makeText(context, "Error taking picture", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ScanViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == -1) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.imageCapture.setImageBitmap(imageBitmap)
+        } else {
+            Log.d("ScanFragment", "Error taking picture")
+        }
     }
-
 }
