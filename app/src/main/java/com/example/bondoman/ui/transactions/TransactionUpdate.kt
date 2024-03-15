9a0a54910af3f8020dee238e148a3a1a48195d1d
henry anand <13521004@std.stei.itb.ac.id>
2024-03-16 00:16:45 +0700

    fix : spinner options

--- app/src/main/java/com/example/bondoman/ui/transactions/TransactionUpdate.kt
+++ app/src/main/java/com/example/bondoman/ui/transactions/TransactionUpdate.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.viewModels
+import com.example.bondoman.R
 import com.example.bondoman.database.AppDatabase
 import com.example.bondoman.databinding.FragmentTransactionUpdateBinding
 import com.example.bondoman.repository.TransactionRepository
@@ -25,12 +27,20 @@ class TransactionUpdate : Fragment() {
         val transactionsViewModel: TransactionsViewModel by viewModels {
             TransactionsViewModelFactory(TransactionRepository(AppDatabase.getInstance(requireContext())))
         }
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.income_outcome_options,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.etKategori.adapter = adapter
+        }
         val transactionId = TransactionUpdateArgs.fromBundle(requireArguments()).transactionId
         transactionsViewModel.getTransactionById(transactionId)
         transactionsViewModel.transaction.observe(viewLifecycleOwner) { transaction ->
             binding.etTitle.setText(transaction!!.transaction_name.toString())
             binding.etKategori.setSelection(
-                if (transaction.transaction_category == "Income") 1 else 2
+                if (transaction.transaction_category == "Income") 0 else 1
             )
             binding.etNominal.setText(transaction.transaction_price.toString())
             binding.etLokasi.setText(transaction.transaction_location.toString())
