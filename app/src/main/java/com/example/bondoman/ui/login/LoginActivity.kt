4c718dc15ee1f9c92ebb0398611dcb33824e7cc9
Ahmad Nadil <13521024@std.stei.itb.ac.id>
2024-03-15 19:22:38 +0700

    style: refined login page

--- app/src/main/java/com/example/bondoman/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/bondoman/ui/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.bondoman.ui.login
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bondoman.MainActivity
@@ -19,6 +20,7 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -55,6 +57,7 @@ class LoginActivity : AppCompatActivity() {
                     Log.e("Login", "Error: ${e.message}")
                     Toast.makeText(this@LoginActivity, "User not found, please try again", Toast.LENGTH_SHORT).show()
                     binding.etPassword.text?.clear()
+                    binding.tvError.visibility = View.VISIBLE
                 }
             }
         }
