32128c2ec6ebf35d4ee671ea4bd795d596e02a6d
MHEN007 <13521007@std.stei.itb.ac.id>
2024-03-15 14:44:15 +0700

    refactor: Singleton Pattern

--- app/src/main/java/com/example/bondoman/api/RetrofitClient.kt
+++ app/src/main/java/com/example/bondoman/api/RetrofitClient.kt
@@ -1,14 +1,20 @@
 package com.example.bondoman.api
 
+import retrofit2.Retrofit
+
 class RetrofitClient {
     companion object {
-        private val retrofit = retrofit2.Retrofit.Builder()
-            .baseUrl("https://pbd-backend-2024.vercel.app/")
-            .addConverterFactory(retrofit2.converter.gson.GsonConverterFactory.create())
-            .build()
+        @Volatile
+        private var retrofit: Retrofit? = null
 
         fun getInstance(): retrofit2.Retrofit {
-            return retrofit
+            if (retrofit == null) {
+                retrofit = retrofit2.Retrofit.Builder()
+                    .baseUrl("https://pbd-backend-2024.vercel.app/")
+                    .addConverterFactory(retrofit2.converter.gson.GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit!!
         }
     }
 }
