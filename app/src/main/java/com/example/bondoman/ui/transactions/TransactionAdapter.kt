6287caf23e8d5ec310a4a52c0aefcb2937b403ec
henry anand <13521004@std.stei.itb.ac.id>
2024-03-16 15:30:01 +0700

    feat : add delete transaction

--- app/src/main/java/com/example/bondoman/ui/transactions/TransactionAdapter.kt
+++ app/src/main/java/com/example/bondoman/ui/transactions/TransactionAdapter.kt
@@ -2,13 +2,19 @@ package com.example.bondoman.ui.transactions
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bondoman.database.Transaction
 import com.example.bondoman.databinding.TransactionItemBinding
 
-class TransactionAdapter(private val data: List<Transaction>,
-                         private val onItemClick: (Int) -> Unit):
-    RecyclerView.Adapter<TransactionAdapter.ViewHolder>()  {
+class TransactionAdapter(private val data: MutableList<Transaction>,
+                         private val fragmentManager: FragmentManager,
+                         private val viewModel: TransactionsViewModel,
+                         private val onItemClick: (Int) -> Unit
+                         ):
+    RecyclerView.Adapter<TransactionAdapter.ViewHolder>(),
+    ConfirmationModal.ConfirmationDialogListener{
+    private var currentItemPosition: Int? = null
     inner class ViewHolder(private val binding: TransactionItemBinding): RecyclerView.ViewHolder(binding.root) {
 
         fun bind(transaction: Transaction) {
@@ -17,7 +23,12 @@ class TransactionAdapter(private val data: List<Transaction>,
             binding.tvKategori.text = transaction.transaction_category
             binding.Location.text = transaction.transaction_location
             binding.price.text = transaction.transaction_price.toString()
-
+            binding.btnDelete.setOnClickListener {
+                val confirmationDialog = ConfirmationModal()
+                currentItemPosition = adapterPosition
+                confirmationDialog.setConfirmationDialogListener(this@TransactionAdapter)
+                confirmationDialog.show(fragmentManager, "ConfirmationDialog")
+            }
             itemView.setOnClickListener {
                 onItemClick(transaction.transaction_id ?: 0)
             }
@@ -40,5 +51,15 @@ class TransactionAdapter(private val data: List<Transaction>,
     }
     override fun getItemCount(): Int {
         return data.count()
+    }
+
+    override fun onDialogPositiveClick() {
+        currentItemPosition?.let { position ->
+            val transactionToDelete = data[position]
+            viewModel.deleteTransaction(transactionToDelete)
+            data.removeAt(position)
+            notifyItemRemoved(position)
+            currentItemPosition = null
+        }
     }
 }
--- app/src/main/java/com/example/bondoman/ui/transactions/TransactionsFragment.kt
+++ app/src/main/java/com/example/bondoman/ui/transactions/TransactionsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,12 +20,18 @@ class TransactionsFragment : Fragment() {
 
     private var _binding: FragmentTransactionsBinding? = null
     private val binding get() = _binding!!
-
+    private lateinit var viewModel : TransactionsViewModel
     private lateinit var rv: RecyclerView
     private lateinit var addTransactionButton: FloatingActionButton
 
     private var listOfTransaction: MutableList<Transaction> = mutableListOf()
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(
+            requireActivity(),
+            TransactionsViewModel.FACTORY
+        )[TransactionsViewModel::class.java]
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -37,14 +44,13 @@ class TransactionsFragment : Fragment() {
 
         viewLifecycleOwner.lifecycleScope.launch {
             val transactions = AppDatabase.getInstance(requireContext()).transactionDao().index("13521007@std.stei.itb.ac.id")
-            val transactionAdapter = TransactionAdapter(transactions) { transactionId ->
+            val transactionAdapter = TransactionAdapter(transactions.toMutableList(), requireActivity().supportFragmentManager, viewModel) { transactionId ->
                 val action = TransactionsFragmentDirections.actionNavigationTransactionsToTransactionUpdate(transactionId)
                 findNavController().navigate(action)
             }
             binding.rvTransaction.adapter = transactionAdapter
             binding.rvTransaction.layoutManager = LinearLayoutManager(requireContext())
         }
-
         addTransactionButton = binding.fabAddTransaction
         addTransactionButton.setOnClickListener{
             val action = TransactionsFragmentDirections.actionNavigationTransactionsToTransactionAdd()
