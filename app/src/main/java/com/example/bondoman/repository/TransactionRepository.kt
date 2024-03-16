c67c01471cf517eee2455e217a532893d1d3603c
henry anand <13521004@std.stei.itb.ac.id>
2024-03-16 13:26:48 +0700

    feat : connect update transaction

--- app/src/main/java/com/example/bondoman/repository/TransactionRepository.kt
+++ app/src/main/java/com/example/bondoman/repository/TransactionRepository.kt
@@ -2,9 +2,16 @@ package com.example.bondoman.repository
 
 import com.example.bondoman.database.AppDatabase
 import com.example.bondoman.database.Transaction
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class TransactionRepository(private val appDatabase: AppDatabase) {
     suspend fun getTransactionById(transactionId: Int?): Transaction?{
         return appDatabase.transactionDao().getTransactionById(transactionId!!)
+    }
+    suspend fun updateTransaction(transaction: Transaction) {
+        withContext(Dispatchers.IO) {
+            appDatabase.transactionDao().update(transaction)
+        }
     }
 }
