5af94948bb114f710a0f16e9b1352e04bcc496a2
henry anand <13521004@std.stei.itb.ac.id>
2024-03-17 21:17:27 +0700

    feat : add repository function for chart

--- app/src/main/java/com/example/bondoman/repository/AppRepository.kt
+++ app/src/main/java/com/example/bondoman/repository/AppRepository.kt
@@ -2,6 +2,7 @@ package com.example.bondoman.repository
 
 import com.example.bondoman.database.AppDatabase
 import com.example.bondoman.database.Transaction
+import com.example.bondoman.database.TransactionSummary
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.Dispatcher
@@ -10,10 +11,16 @@ class AppRepository(private val appDatabase: AppDatabase) {
     suspend fun getTransactionById(transactionId: Int?): Transaction?{
         return appDatabase.transactionDao().getTransactionById(transactionId!!)
     }
+    suspend fun getTransactionsByEmail(email: String): List<Transaction> {
+        return appDatabase.transactionDao().index(email)
+    }
     suspend fun updateTransaction(transaction: Transaction) {
         withContext(Dispatchers.IO) {
             appDatabase.transactionDao().update(transaction)
         }
+    }
+    suspend fun getLast7Transaction(email: String, category: String) : List<TransactionSummary>{
+        return appDatabase.transactionDao().getLast7Transaction(email,category)
     }
 
     suspend fun deleteTransaction(transaction: Transaction){
